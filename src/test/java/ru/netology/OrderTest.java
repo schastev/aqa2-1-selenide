9c8dd86b2a1f67389ef0e1ef389765cadffbe634
k-emiko <62057364+k-emiko@users.noreply.github.com>
2021-05-23 10:52:16 +1100

    address the other comment

--- src/test/java/ru/netology/OrderTest.java
+++ src/test/java/ru/netology/OrderTest.java
@@ -10,10 +10,10 @@ public class OrderTest {
     @Test
     public void correctInputTest(){
         open("http://localhost:9999");
-        $("[type='tel']").setValue("+00000000000");
-        $("[name='name']").setValue("Аа-бБ вВ");
+        $("[data-test-id='phone'] .input__control").setValue("+00000000000");
+        $("[data-test-id='name'] .input__control").setValue("Аа-бБ вВ");
         $("[data-test-id='agreement']").click();
-        $("[type='button']").click();
+        $(".button").click();
         $(".paragraph")
                 .shouldHave(Condition.exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
     }
@@ -21,9 +21,9 @@ public class OrderTest {
     @Test
     public void emptyNameTest() {
         open("http://localhost:9999");
-        $("[type='tel']").setValue("+00000000000");
+        $("[data-test-id='phone'] .input__control").setValue("+00000000000");
         $("[data-test-id='agreement']").click();
-        $("[type='button']").click();
+        $(".button").click();
         $(".input_invalid .input__sub")
                 .shouldHave(Condition.text("Поле обязательно для заполнения"));
     }
@@ -31,38 +31,38 @@ public class OrderTest {
     @Test
     public void incorrectNameTest() {
         open("http://localhost:9999");
-        $("[name='name']").setValue("123");
-        $("[type='tel']").setValue("+00000000000");
+        $("[data-test-id='name'] .input__control").setValue("123");
+        $("[data-test-id='phone'] .input__control").setValue("+00000000000");
         $("[data-test-id='agreement']").click();
-        $("[type='button']").click();
+        $(".button").click();
         $(".input_invalid .input__sub")
                 .shouldHave(Condition.text("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
     }
     @Test
     public void emptyPhoneTest() {
         open("http://localhost:9999");
-        $("[name='name']").setValue("Аа-бБ вВ");
+        $("[data-test-id='name'] .input__control").setValue("Аа-бБ вВ");
         $("[data-test-id='agreement']").click();
-        $("[type='button']").click();
+        $(".button").click();
         $(".input_invalid .input__sub")
                 .shouldHave(Condition.text("Поле обязательно для заполнения"));
     }
     @Test
     public void incorrectPhoneTest() {
         open("http://localhost:9999");
-        $("[type='tel']").setValue("123");
-        $("[name='name']").setValue("Аа-бБ вВ");
+        $("[data-test-id='phone'] .input__control").setValue("123");
+        $("[data-test-id='name'] .input__control").setValue("Аа-бБ вВ");
         $("[data-test-id='agreement']").click();
-        $("[type='button']").click();
+        $(".button").click();
         $(".input_invalid .input__sub")
                 .shouldHave(Condition.text("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
     }
     @Test
     public void noAgreementTest() {
         open("http://localhost:9999");
-        $("[type='tel']").setValue("+00000000000");
-        $("[name='name']").setValue("Аа-бБ вВ");
-        $("[type='button']").click();
+        $("[data-test-id='phone'] .input__control").setValue("+00000000000");
+        $("[data-test-id='name'] .input__control").setValue("Аа-бБ вВ");
+        $(".button").click();
         $(".input_invalid")
                 .shouldHave(Condition.text("Я соглашаюсь с условиями обработки и использования моих персональных данных и разрешаю сделать запрос в бюро кредитных историй"));
     }
@@ -70,7 +70,7 @@ public class OrderTest {
     public void twoIncorrectFieldsFirstTest() {
         open("http://localhost:9999");
         $("[data-test-id='agreement']").click();
-        $("[type='button']").click();
+        $(".button").click();
         $(".input_invalid .input__sub")
                 .shouldHave(Condition.text("Поле обязательно для заполнения"));
     }
